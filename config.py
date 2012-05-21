5cf2a586acb646894e984f41aba72a944dc1c34f
Clodo <claudiobidau@gmail.com>
2012-05-21 16:32:58 -0300

    borrando archivos innecesarios

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-_basedir = os.path.abspath(os.path.dirname(__file__))
-
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'db/db.sqlite')
-BABEL_DEFAULT_LOCALE = 'es_AR'
-BABEL_DEFAULT_TIMEZONE = 'America/Argentina/Buenos_Aires'
