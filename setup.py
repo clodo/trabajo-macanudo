10c0ddfbb10b5d18f3e9da2461a7f2c3a64104b7
Clodo <claudiobidau@gmail.com>
2012-05-22 01:51:46 -0300

    connection string

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+  name='Trabajo Macanudo',
+  version='0.1',
+  description='Trabajo Macanudo',
+  author='Banquito',
+  author_email='hola@elgalpondebanquito.com.ar',
+  packages=['frontend'],
+  include_packages_data=True,
+  zip_safe=False,
+  install_requires=[
+    'Flask',
+    'Flask-SQLAlchemy',
+    'Flask-WTF',
+    'Flask-Script',
+    'Flask-Babel',
+    'Flask-Testing',
+    'nose',
+  ]
+)
